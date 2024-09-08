2560b8a58dee3b9986c6b6518c78e72188ee3b90
unnatiS20 <unnatisharma106@gmail.com>
2024-09-08 20:42:47 +0530

    constant cpp

---
+++ Complexities/constant.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare two integers
+    int a = 5, b = 10;
+
+    // Perform addition in constant time
+    int sum = a + b;
+
+    // Output the result
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+
+    return 0;
+}
